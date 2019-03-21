85991bdffb16a1ad6f028d25745b1c31ed666a8d
guy arbily <guy.arbilly@gmail.com>
2019-03-21 15:05:16 +0200

    add system variable for mongo DB client

--- src/common/database.py
+++ src/common/database.py
@@ -1,8 +1,11 @@
+import os
+
 import pymongo
 
 
 class Database(object):
-    URI = "mongodb://127.0.0.1:27017"
+    URI = os.environ.get("MONGOLAB_URI")
+     #                    ""mongodb://127.0.0.1:27017"
     DATABASE = None
 
     @staticmethod
