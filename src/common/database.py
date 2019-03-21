fe8b43039a3104aa95a618e39ca2c55f509bb652
guy arbily <guy.arbilly@gmail.com>
2019-03-21 21:25:30 +0200

    trying differnt method for database to use defalut name from uri

--- src/common/database.py
+++ src/common/database.py
@@ -4,7 +4,7 @@ import pymongo
 
 
 class Database(object):
-    URI = os.environ.get("MONGOLAB_URI")
+    URI = os.environ.get("MONGODB_URI")
      #                    "mongodb://127.0.0.1:27017"
     DATABASE = None
 
